25ef522c2204e78a6b05308465e5d020c601ab3a
ddaci <ddaci@DESKTOP-IVLUD9Q>
2022-12-04 16:43:58 +0200

    Add project files.

---
+++ Tomoiahg_Cristian Dacian_Lab1/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tomoiahg_Cristian_Dacian_Lab1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
